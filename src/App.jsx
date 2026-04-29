19f7d7edef22212db58a91f148e2c0403a37bc5a
ნიკოლოზი ელიზბარაშვილი <nelizbarashvili@local.emis.ge>
2026-04-29 11:03:38 +0400

    use Effect

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,38 @@ import './App.css'
 import Header from './Layout/Header'
 import { Route, Routes } from 'react-router'
 import Contact from './Pages/Contact'
+import { useEffect, useState } from 'react'
+import { API_URL } from './config'
 
 function App() {
+
+  const [posts, setPosts] = useState([]);
+  const [counter, setCounter] = useState(0);
+
+  useEffect(()=>{
+    const getPosts = async () => {
+        const response = await fetch(`${API_URL}/posts`);
+        const data = await response.json();
+        setPosts(data);
+        console.log(data)
+    }
+    getPosts();
+  },[])
+
   
+
+
 
   return (
     <>
      <Header />
+
+     {counter}
+
+   <button onClick={() => setCounter(prev=> prev+1)}>show posts</button>   
+    
+    <div>{posts.map( (item) => { return (<div key={item.id}>{item.title}</div>)} ) }</div>
+
      <Routes>
       <Route path='/' element={ <h1>Home Page</h1> } />
       <Route path='/contact' element={ <Contact /> } />
